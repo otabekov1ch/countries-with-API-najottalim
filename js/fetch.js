1625b992c13cbbcf2b3346e208963dda4d70895a
otabekov1ch <88477479+otabekov1ch@users.noreply.github.com>
2022-06-23 16:35:39 +0500

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,7 @@ var countriesData = []
 fetch('https://restcountries.com/v3.1/all')
 .then(res => {res.json()
     .then(data => {
-        countriesData = data.slice(0, 50);
+        countriesData = data;
         renderCountries(countriesData, elCountriesList)
     })
 });
